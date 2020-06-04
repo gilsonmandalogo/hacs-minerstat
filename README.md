456d872cbeb6d8e5ef9cc508cf7d9bcb04f38ac1
Gilson Marquato Júnior <gilsonmandalogo@hotmail.com>
2020-06-04 01:06:52 +0000

    Release of first version.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # hacs-minerstat
+
+A integration with [Minerstart](https://minerstat.com/) to create a sensor from your rig's hashrate.
+
+## Usage
+`configuration.yaml`:
+```yaml
+sensor:
+  - platform: hacs-minerstat
+    name: "My Awesome Rig"
+    access_key: "00000000"
+    rig_name: "RIG1"
+```
+
+## Options
+|Name|Type|Necessity|Default|Description|
+|----|:--:|:-------:|:-----:|-----------|
+|`platform`|string|**Required**|`hacs-minerstat`|The platform name|
+|`access_key`|string|**Required**||Your personal access key from https://my.minerstat.com/|
+|`rig_name`|string|**Required**||The name that you defined for your rig at Minerstat|
+|`name`|string|Optional|`Minerstat`|Custom name for the sensor|
--- custom_components/hacs-minerstat/sensor.py
+++ custom_components/hacs-minerstat/sensor.py
@@ -5,11 +5,14 @@ import voluptuous as vol
 import urllib.request, json
 import homeassistant.helpers.config_validation as cv
 
+__version__ = '1.0.0'
+
 CONF_NAME = 'name'
 CONF_ACCESS_KEY = 'access_key'
 CONF_RIG_NAME = 'rig_name'
 DEFAULT_NAME = 'Minerstat'
 DEFAULT_SCAN_INTERVAL = timedelta(minutes=15)
+SCAN_INTERVAL = timedelta(minutes=15)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
@@ -17,32 +20,39 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_RIG_NAME): str
 })
 
+def setup_platform(hass, config, add_devices, discovery_info=None):
+    add_devices([Minerstat(hass, config)])
+
 class Minerstat(entity.Entity):
     def __init__(self, hass, config):
         self.hass = hass
         self._config = config
-        # self._state = None
-        self._hashrate = None
+        self._state = None
         self._unit = None
-
-    # @property
-    # def state(self):
-    #     return self._state
+        self.update()
 
     @property
     def name(self):
         return self._config[CONF_NAME]
 
+    @property
+    def icon(self):
+        return 'mdi:bitcoin'
+
+    @property
+    def state(self):
+        return self._state
+
     def update(self):
-        with urllib.request.urlopen(f'https://api.minerstat.com/v2/stats/{self._config[CONF_ACCESS_KEY]}') as url:
+        req = urllib.request.Request(f'https://api.minerstat.com/v2/stats/{self._config[CONF_ACCESS_KEY]}/{self._config[CONF_RIG_NAME]}', headers={'User-Agent' : "Home-assistant.io"})
+        with urllib.request.urlopen(req) as url:
             data = json.loads(url.read().decode())
 
-            self._hashrate = data[self._config[CONF_RIG_NAME]]['mining']['hashrate']['hashrate']
+            self._state = data[self._config[CONF_RIG_NAME]]['mining']['hashrate']['hashrate']
             self._unit = data[self._config[CONF_RIG_NAME]]['mining']['hashrate']['hashrate_unit']
 
     @property
     def device_state_attributes(self):
         return {
-            'hashrate': self._hashrate,
-            'unit': self._unit
+            'unit_of_measurement': self._unit
         }
